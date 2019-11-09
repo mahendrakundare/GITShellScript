602030548a5d1fc752ecf5a0181a46a08d2cfdd9
mvinchankar <mvinchankar18@gmail.com>
2019-11-09 18:22:17 +0530

    wages added

--- empFinal.sh
+++ empFinal.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -x
-employeeCheck=$((RANDOM%2));
-if [ ${employeeCheck} == 1 ];
+randomCheck=$((RANDOM%3));
+ratePerHour=100;
+if [ ${randomCheck} == 1 ];
 then
+
+	echo FULL TIME SALARY;
+	echo employee is present;
 	empHour=8;
-elif [ ${employeeCheck} == 2 ];
+elif [ ${randomCheck} == 2 ];
 then
+	echo PART TIME SALARY;
 	empHour=4
+	echo monthly parttime salary is 
 else
 	empHour=0;
+	echo employee is not present;
 fi
-
+Salary=$(($ratePerHour*$empHour));
+echo $Salary
