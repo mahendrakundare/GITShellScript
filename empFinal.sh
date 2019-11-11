cd38377c55462c91f2b55f70cf7968a5b0728735
mvinchankar <mvinchankar18@gmail.com>
2019-11-11 14:55:13 +0530

    Array added

--- empFinal.sh
+++ empFinal.sh
@@ -1,21 +1,36 @@
 #!/bin/bash -x
-employeeCheck=$((RANDOM%3))
-isFullTime=1;
-isPartTime=2;
-isAbsent=0;
-ratePerHour=200;
-valid=true;
-NUM_WORKING_DAYS=20;
-totalWorkingHours=0;
-totalWorkingDays=0;
 
-function getWorkingHours(){
-	case $1 in $isFullTime )
-	workhours=8;;
-		in $isPartTime )
-	workhours=4;;
-			* )
-	workhours=0;;
-	esac
-	echo $workhours;
+rate_hour=300;
+isPartTime=2;
+isFullTime=1;
+isAbsent=0;
+Salary=0;
+counter=0;
+working_hours=0;
+hours=0;
+function total_hours(){
+   work=$((RANDOM%3))
+   case $work in $isFullTime )
+          working_hours=8;;
+        $isPartTime )
+          working_hours=5;;
+        $isAbsent )
+          working_hours=0;;
+   esac
+   echo $working_hours
 }
+function Wage(){
+for (( day=1; day<=5; day++ ))
+do
+   
+   hours=$( total_hours )
+   temp=$(($rate_hour * $hours))
+   Salary=$(($Salary+$temp))
+   Wages[((counter++))]=$temp
+   
+done
+echo ${Wages[@]}
+echo $Salary
+}
+
+result=$( Wage )
